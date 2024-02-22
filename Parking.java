f6b063d22732c08f54db8eca5f2c21fac7a9e5a3
Aravind <aravind16101800@gmail.com>
2024-02-22 22:38:57 +0530

    feat: parkingCar function added

---
+++ Parking.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Parking {
+    static int slots;
+    static int availabaleSlot;
+    static ArrayList<Integer> parkingSlot = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total number of parking slots: ");
+        slots = sc.nextInt();
+        availabaleSlot = slots;
+
+        while(true){
+            System.out.println("Enter your choice: ");
+            System.out.println("1.Park a car");
+            System.out.println("2.Remove a car");
+            System.out.println("3.View parked car");
+            System.out.println("4.Exit");
+
+            int input = sc.nextInt();
+            switch (input){
+                case 1:
+                    parkCar();
+                    break;
+                case 2:
+                    removeCar();
+                    break;
+                case 3:
+                    viewParkedCars();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Enter a valid input");
+            }
+        }
+    }
+
+    private static void parkCar() {
+        Scanner sc = new Scanner(System.in);
+        // if parking lot is full already
+        if(availabaleSlot == 0){
+            System.out.println("The parking lot is already full");
+        }
+        System.out.println("Enter the license plate number of car: ");
+        int license_no = sc.nextInt();
+        parkingSlot.add(license_no);
+        System.out.print("Car parked Successfully. ");
+        availabaleSlot--;
+        System.out.println("Available slots: "+availabaleSlot);
+    }
+
+    
+}
